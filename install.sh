a2b76e8e39d815c17c22c110ed1d102198f11cb9
prasanna8585 <prasannadabiroc@gmail.com>
2024-02-25 23:56:28 +0530

    First Commit

---
+++ install.sh
@@ -0,0 +1,67 @@
+#!bin/bash
+
+. ./config.cfg
+sudo apt-get update
+
+sudo apt-get install -y libcurl4-openssl-dev
+sudo apt-get install -y libssl-dev
+sudo apt-get install -y jq
+sudo apt-get install -y ruby-full
+sudo apt-get install -y libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev build-essential libgmp-dev zlib1g-dev
+sudo apt-get install -y build-essential libssl-dev libffi-dev python-dev
+sudo apt-get install -y python-setuptools
+sudo apt-get install -y libldns-dev
+sudo apt-get install -y python3-pip
+sudo apt-get install -y python-pip
+sudo apt-get install -y python-dnspython
+sudo apt-get install -y git
+sudo apt-get install -y rename
+
+if [[ -z "$GOPATH" ]];then
+echo "It looks like go is not installed, would you like to install it now"
+PS3="Please select an option : "
+choices=("yes" "no")
+select choice in "${choices[@]}"; do
+        case $choice in
+                yes)
+
+					echo "Installing Golang"
+					wget https://dl.google.com/go/go1.18.5.linux-amd64.tar.gz
+					sudo tar -xvf go1.18.5.linux-amd64.tar.gz
+					sudo rm -rf /usr/local/go
+					sudo mv go /usr/local
+					export GOROOT=/usr/local/go
+					export GOPATH=$HOME/go
+					export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+					echo 'export GOROOT=/usr/local/go' >> ~/.bash_profile
+					echo 'export GOPATH=$HOME/go'	>> ~/.bash_profile			
+					echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bash_profile	
+					source ~/.bash_profile
+					sleep 1
+					break
+					;;
+				no)
+					echo "Please install go and rerun this script"
+					echo "Aborting installation..."
+					exit 1
+					;;
+	esac	
+done
+fi
+
+#create a tools folder in ~/
+mkdir ~/tools
+cd ~/tools/
+#
+#
+echo "Installing subfinder"
+go install github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+echo "#######################################################################################################################################################################"
+
+echo "installing waybackurls"
+go install github.com/tomnomnom/waybackurls@latest
+echo "#######################################################################################################################################################################"
+
+echo "installing httpx"
+go install github.com/projectdiscovery/httpx/cmd/httpx@latest
+echo "done"
